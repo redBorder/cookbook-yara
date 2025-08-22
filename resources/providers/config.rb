19758971400cb942d85db42dfacd2aad34a0b801
Pablo Torres <ptorres@redborder.com>
2025-08-22 16:36:03 +0100

    Improvement #22559: Create yara_rules folder, node attributes and yara_loader file

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -10,6 +10,25 @@ action :add do
       action :upgrade
     end
 
+    directory "/usr/share/logstash/yara_rules" do
+      owner "webui"
+      group "webui"
+      mode 0755
+      action :create
+    end
+
+    # Uncomment when rb-sequence-oozie is merged
+    # template '/var/rb-sequence-oozie/workflow/yara_loader.yml' do
+    #   cookbook 'yara'
+    #   source 'yara_loader.yml.erb'
+    #   owner root
+    #   group root
+    #   mode '0644'
+    #   retries 2
+    #   notifies :restart, 'service[rb-sequence-oozie]'
+    # end
+
+
     Chef::Log.info('Yara cookbook has been processed')
   rescue => e
     Chef::Log.error(e.message)
