f41df731ff75e5497854c5c7212cd22f192c6261
Pablo Torres <ptorres@redborder.com>
2025-08-25 10:37:27 +0100

    Improvement #22559: Fix lint 2

--- resources/attributes/default.rb
+++ resources/attributes/default.rb
@@ -7,21 +7,21 @@ default['redborder']['manager']['yara']['severity']['scores']['medium'] = 0.3
 default['redborder']['manager']['yara']['severity']['scores']['low'] = 0.1
 
 default['redborder']['manager']['yara']['severity']['high'] = {}
-default['redborder']['manager']['yara']['severity']['high']['high_lower_threshold']  = 10
+default['redborder']['manager']['yara']['severity']['high']['high_lower_threshold'] = 10
 default['redborder']['manager']['yara']['severity']['high']['low_upper_threshold'] = 5
 default['redborder']['manager']['yara']['severity']['high']['high_score'] = 100
 default['redborder']['manager']['yara']['severity']['high']['medium_score'] = 60
 default['redborder']['manager']['yara']['severity']['high']['low_score'] = 30
 
 default['redborder']['manager']['yara']['severity']['medium'] = {}
-default['redborder']['manager']['yara']['severity']['medium']['high_lower_threshold']  = 10
+default['redborder']['manager']['yara']['severity']['medium']['high_lower_threshold'] = 10
 default['redborder']['manager']['yara']['severity']['medium']['low_upper_threshold'] = 5
 default['redborder']['manager']['yara']['severity']['medium']['high_score'] = 100
 default['redborder']['manager']['yara']['severity']['medium']['medium_score'] = 60
 default['redborder']['manager']['yara']['severity']['medium']['low_score'] = 30
 
 default['redborder']['manager']['yara']['severity']['low'] = {}
-default['redborder']['manager']['yara']['severity']['low']['high_lower_threshold']  = 10
+default['redborder']['manager']['yara']['severity']['low']['high_lower_threshold'] = 10
 default['redborder']['manager']['yara']['severity']['low']['low_upper_threshold'] = 5
 default['redborder']['manager']['yara']['severity']['low']['high_score'] = 100
 default['redborder']['manager']['yara']['severity']['low']['medium_score'] = 60
